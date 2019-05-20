fabbdce5e32e421d5d02b19fd0aeb0855700f94d
Arthur Chen <arthurchen@bluetechnology.com.tw>
2019-05-20 16:58:54 +0800

    update

--- cleanZip7z from download.bat
+++ cleanZip7z from download.bat
@@ -1,4 +1,8 @@
 del /F /S C:\Users\Arthur\Downloads\*.zip 
 del /F /S C:\Users\Arthur\Downloads\*.7z
+forfiles /p C:\Users\Arthur\Downloads /d -200 /c "cmd /c del /s /q @path"
+
 del /F /S D:\¤U¸ü\*.zip 
 del /F /S D:\¤U¸ü\*.7z
+forfiles /p D:\¤U¸ü /d -200 /c "cmd /c del /s /q @path"
+pause
